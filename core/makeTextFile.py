d79f4a879faa736250054069e561fa133833d14a
shupeng.lisp <shupeng.lisp@gmail.com>
2012-08-01 23:03:48 +0800

    ch3 src of core python

---
+++ core/makeTextFile.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+# makeTextFile.py -- create text file
+
+import os
+ls = os.linesep
+
+# get filename
+while True:
+    fname = input('Enter file name:')
+    if os.path.exists(fname):
+        print ('Error: "%s" already exists' % fname)
+    else:
+        break
+
+# get file content (text) lines
+all = []
+print ('\nEnter lines ("." by itself to quit).\n')
+
+# loop until user terminates input
+while True:
+    entry = input('>')
+    if entry == '.':
+        break
+    else:
+        all.append(entry)
+
+# write lines to file with proper line-ending
+fobj = open(fname, 'w')
+#fobj.writelines(['%s%s' % (x, ls) for x in all])
+fobj.write('\n'.join(all))
+fobj.close()
+print ('DONE!')
